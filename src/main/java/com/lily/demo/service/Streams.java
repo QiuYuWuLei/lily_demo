f5ed8cf06c20cfbabe82e6ad942ce063db3e3772
fangl <1048780267@qq.com>
2019-04-08 16:51:58 +0800

    fl

--- src/main/java/com/lily/demo/service/Streams.java
+++ src/main/java/com/lily/demo/service/Streams.java
@@ -41,7 +41,7 @@ public class Streams {
         map.put("f", "f");
         map.put("c", "c");
         map.put("d", "d");
-        map.put("e", "e");
+        map.put("1", "1");
         String jsonStr = gson.toJson(map);
 
         System.out.printf(jsonStr);
